462627fb26993027f1a477cfe7c7db723a804a36
Petra Vos <petravos70@gmail.com>
2018-10-28 09:04:32 +0100

    include gitignore

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fded2a26df6524e3d5a7";
+/******/ 	var hotCurrentHash = "3e8ee18f9f8594633ed1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
