df8ffed8780a6054ec41bb260a2b40dae30d133d
Petra Vos <petravos70@gmail.com>
2018-10-29 20:08:17 +0100

    Add styling to page

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Meta from "./Meta";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
+
+const theme = {
+  green: "#55d6be",
+  black: "#393939",
+  grey: "#3a3a3a",
+  lightgrey: "#e1e1e1",
+  offwhite: "#ededed",
+  maxwidth: "1000px",
+  bs: "0 12px 24px 0 rgb(0,0,0,0.09)"
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxwidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
