e3255bd83c13ecffdba9ff6bb8e88e0c4a4e1f12
Petra Vos <petravos70@gmail.com>
2018-11-04 19:15:56 +0100

    Add prisma to files

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -35,7 +35,7 @@ const Logo = styled.h1`
 
 const StyledHeader = styled.header`
   .bar {
-    border-bottom: 10px solid ${props => props.theme.black};
+    border-bottom: 1px solid ${props => props.theme.black};
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
