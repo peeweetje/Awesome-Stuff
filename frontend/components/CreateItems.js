0920066c18db5408cace14d3affe2f697cfa34f7
Petra Vos <petravos70@gmail.com>
2018-11-22 13:01:12 +0100

    Adding fieldsets and state to page

--- frontend/components/CreateItems.js
+++ frontend/components/CreateItems.js
@@ -5,10 +5,66 @@ import Form from "./styles/Form";
 import formatMoney from "../lib/formatMoney";
 
 class CreateItems extends Component {
+  state = {
+    title: "Red dead redemption",
+    description: "I love black ops 4",
+    image: "noah.jpg",
+    largeImage: "large-noah.jpg",
+    price: 175
+  };
+
+  handleChange = e => {
+    const { name, type, value } = e.target;
+    const val = type === "number" ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+
   render() {
     return (
-      <Form>
-        <h2>Sell an Item</h2>
+      <Form
+        onSubmit={e => {
+          e.preventDefault();
+          console.log(this.state);
+        }}
+      >
+        <fieldset>
+          <label htmlFor="title">
+            Title
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="title"
+              required
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="price">
+            Price
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="price"
+              required
+              value={this.state.price}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="description">
+            Description
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Enter a description"
+              required
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </fieldset>
       </Form>
     );
   }
