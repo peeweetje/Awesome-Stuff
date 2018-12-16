c63e2579ccee330af4eef89deeba907a57342a9b
Petra Vos <petravos70@gmail.com>
2018-12-16 19:33:56 +0100

    Add new component and page for single items

---
+++ frontend/components/SingleItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SingleItem extends Component {
+  render() {
+    return (
+      <div>
+        <p>Single item component!!!!</p>
+      </div>
+    );
+  }
+}
---
+++ frontend/pages/item.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SingleItem from "../components/SingleItem";
+
+const item = props => {
+  return (
+    <div>
+      <SingleItem />
+    </div>
+  );
+};
+
+export default item;
