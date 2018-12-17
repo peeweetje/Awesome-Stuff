c7e19061a55fbf6753ea58b7bfcc5b07565f1fdd
Petra Vos <petravos70@gmail.com>
2018-12-17 12:30:55 +0100

    Refactor: add styling to single items

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -2,6 +2,27 @@ import React, { Component } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import Error from "./ErrorMessage";
+import styled from "styled-components";
+import Head from "next/head";
+
+const SingleItemStyles = styled.div`
+  max-width: 1200px;
+  margin: 2rem auto;
+  box-shadow: ${props => props.theme.bs};
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  min-height: 800px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
+`;
 
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
@@ -28,7 +49,19 @@ class SingleItem extends Component {
           if (loading) return <p>Loading...</p>;
           if (!data.item) return <p>No item found for {this.props.id}</p>;
           console.log(data);
-          return <p>Single item component!!!!{this.props.id}</p>;
+          const item = data.item;
+          return (
+            <SingleItemStyles>
+              <Head>
+                <title>Awesome Stuff | {item.title}</title>
+              </Head>
+              <div className="details">
+                <h2>Viewing {item.title}</h2>
+                <p>{item.description}</p>
+              </div>
+              <img src={item.largeImage} alt={item.title} />
+            </SingleItemStyles>
+          );
         }}
       </Query>
     );
