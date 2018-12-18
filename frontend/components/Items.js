2e1370953d923ca77bc79e5a55821633c0227dc9
Petra Vos <petravos70@gmail.com>
2018-12-18 13:25:28 +0100

    Refactor: add pagination to items file

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -3,6 +3,7 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import styled from "styled-components";
 import Item from "./Item";
+import Pagination from "./Pagination";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -33,6 +34,7 @@ export default class Items extends Component {
   render() {
     return (
       <Center>
+        <Pagination />
         <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
@@ -47,6 +49,7 @@ export default class Items extends Component {
             );
           }}
         </Query>
+        <Pagination />
       </Center>
     );
   }
