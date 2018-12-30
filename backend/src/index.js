e4d82b449399a9f822ee589cd1c2f217637d33b3
Petra Vos <petravos70@gmail.com>
2018-12-30 14:33:05 +0100

    Add middleware te get userId on request. Add me query

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
@@ -8,7 +9,16 @@ const server = createServer();
 
 //use express middlware to handle cookies (JWT)
 server.express.use(cookieParser());
-// TODO use express middlware to populate current user
+// decode the jwt to get the user id on every request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the request for future request to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,11 +3,19 @@ const { forwardTo } = require("prisma-binding");
 const Query = {
   items: forwardTo("db"),
   item: forwardTo("db"),
-  itemsConnection: forwardTo("db")
-  //   async items(parent, args, ctx, info) {
-  //     const items = await ctx.db.query.items();
-  //     return items;
-  //   }
+  itemsConnection: forwardTo("db"),
+  me(parent, args, ctx, info) {
+    //Check if there is a userId
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
