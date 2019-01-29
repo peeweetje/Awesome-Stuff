bd29a75f573c5c14f953f62f008b7ccaec5c207d
Petra Vos <petravos70@gmail.com>
2019-01-29 19:59:02 +0100

    Add new component CountCart. Add animation for the count cart

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,6 +4,7 @@ import { TOGGLE_CART_MUTATION } from "./Cart";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
 import SignOut from "./SignOut";
+import CartCount from "./CountCart";
 
 const Nav = () => (
   <User>
@@ -25,7 +26,17 @@ const Nav = () => (
             </Link>
             <SignOut />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+              {toggleCart => (
+                <button onClick={toggleCart}>
+                  My Cart
+                  <CartCount
+                    count={me.cart.reduce(
+                      (tally, cartItem) => tally + cartItem.quantity,
+                      0
+                    )}
+                  />
+                </button>
+              )}
             </Mutation>
           </>
         )}
